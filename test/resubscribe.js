2e37cb240f3623acbe23c4c12a40d96a8ce94181
James Halliday <mail@substack.net>
2012-07-19 01:33:02 -0700

    normalize indents

--- test/resubscribe.js
+++ test/resubscribe.js
@@ -4,29 +4,29 @@ var test = require('tap').test;
 test('resubscribe if connection dies', function(t) {
     var serverPort = Math.floor(Math.random() * 5e4 + 1e4);
     var server = seaport.createServer();
-    var client = seaport.connect('localhost:' + serverPort, {reconnect: 10});
+    var client = seaport.connect('localhost:' + serverPort, { reconnect: 10 });
     
     t.plan(6);
-
+    
     var eventNames = ['allocate', 'assume', 'free'];
-
+    
     eventNames.forEach(function(eventName) {
         client.subscribe(eventName, function() {
             t.ok(true, eventName + ' emitted');
         });
     });
-
+    
     function emit() {
         eventNames.forEach(function(eventName) {
             server.emit(eventName, {});
         });
     }
-
+    
     server.listen(serverPort);
-
+    
     // Wait for client to connect
     setTimeout(emit, 200)
-
+    
     // Wait for first tests to finish
     setTimeout(function() {
         client.up.conn.stream.end();
@@ -34,7 +34,7 @@ test('resubscribe if connection dies', function(t) {
     
     // Run second test
     setTimeout(emit, 1600);
-
+    
     t.on('end', function() {
         server.close();
         client.close();
