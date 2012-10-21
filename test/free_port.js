36f7b0498f66d57842de6a2c05dcf59267306e16
Mike Brevoort <mike@brevoort.com>
2012-10-21 14:45:57 -0600

    fixed ports.free() bug when passed just a number, role wasn't getting unallocated

---
+++ test/free_port.js
@@ -0,0 +1,38 @@
+var test = require('tap').test;
+var seaport = require('../');
+
+// same exact test as free.js but passing ports.free a number parameter
+test('free', function (t) {
+    t.plan(6);
+    var port = Math.floor(Math.random() * 5e4 + 1e4);
+    var server = seaport.createServer();
+    server.listen(port);
+    
+    var ports = seaport.connect('localhost', port);
+    var gotPort, gotRec;
+    server.on('free', function (rec) {
+        t.same(rec, gotRec);
+        t.same(server.allocated[gotRec.host], []);
+        ports.close();
+    });
+    
+    ports.allocate('http', function (p) {
+        t.ok(p >= 10000 && p < 65536);
+        gotPort = p;
+        
+        process.nextTick(function () {
+            ports.query('http', function (ps) {
+                t.equal(ps.length, 1);
+                t.equal(ps[0].host, '127.0.0.1');
+                t.equal(ps[0].port, gotPort);
+                
+                gotRec = ps[0];
+                ports.free(ps[0].port);
+            });
+        });
+    });
+    
+    t.on('end', function () {
+        server.close();
+    });
+});
