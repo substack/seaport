8c93192b69a39edb44901c662ee10c1d8badef4c
James Halliday <mail@substack.net>
2012-11-01 10:11:21 -0700

    free can take just a port now

--- test/free_port.js
+++ test/free_port.js
@@ -1,34 +1,34 @@
 var test = require('tap').test;
 var seaport = require('../');
 
-// same exact test as free.js but passing ports.free a number parameter
 test('free', function (t) {
-    t.plan(5);
+    t.plan(7);
     var port = Math.floor(Math.random() * 5e4 + 1e4);
     var server = seaport.createServer();
     server.listen(port);
     
     var ports = seaport.connect('localhost', port);
-    var gotPort, gotRec;
-    server.on('free', function (rec) {
-        t.same(rec, gotRec);
-        ports.close();
-    });
     
-    ports.register('http', function (p) {
-        t.ok(p >= 10000 && p < 65536);
-        gotPort = p;
+    var p = ports.register('http');
+    t.ok(p >= 10000 && p < 65536);
+    
+    setTimeout(function () {
+        // give time for the 'host' event to fire
         
-        process.nextTick(function () {
-            var ps = ports.query('http');
-            t.equal(ps.length, 1);
-            t.equal(ps[0].host, '127.0.0.1');
-            t.equal(ps[0].port, gotPort);
-            
-            gotRec = ps[0];
-            ports.free(ps[0].port);
+        server.on('free', function (rec) {
+            t.equal(rec.port, ps[0].port);
+            t.equal(rec.host, ps[0].host);
+            t.equal(rec.id, ps[0].id);
+            ports.close();
         });
-    });
+        
+        var ps = ports.query('http');
+        t.equal(ps.length, 1);
+        t.equal(ps[0].host, '127.0.0.1');
+        t.equal(ps[0].port, p);
+        
+        ports.free(ps[0].port);
+    }, 100);
     
     t.on('end', function () {
         server.close();
