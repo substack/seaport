80272b86636a8a43bd5780b3b8ed92216b6d32c8
James Halliday <mail@substack.net>
2012-11-01 09:07:09 -0700

    remove callback from register(), meta_service test passes

--- test/meta_service.js
+++ test/meta_service.js
@@ -2,7 +2,7 @@ var seaport = require('../');
 var test = require('tap').test;
 
 test('meta params in a service', function (t) {
-    t.plan(5);
+    t.plan(4);
     var serverPort = Math.floor(Math.random() * 5e4 + 1e4);
     var server = seaport.createServer();
     server.listen(serverPort);
@@ -14,7 +14,6 @@ test('meta params in a service', function (t) {
     
     var t0 = Date.now();
     ports[0].get('woo', function (ps) {
-        t.ok(Date.now() - t0 >= 100);
         t.equal(ps.length, 1);
         t.equal(ps[0].host, '127.0.0.1');
         t.equal(ps[0].port, gotPort);
@@ -24,10 +23,7 @@ test('meta params in a service', function (t) {
     
     var gotPort;
     setTimeout(function () {
-        ports[1].service('woo', { beep : 'boop' }, function (port, ready) {
-            gotPort = port;
-            setTimeout(ready, 50);
-        });
+        gotPort = ports[1].register('woo', { beep : 'boop' });
     }, 50);
     
     t.on('end', function () {
