f7f5806c01b27b02cf3931928617e3c4a6113f45
James Halliday <mail@substack.net>
2012-11-01 09:59:59 -0700

    update free test, fails

--- test/free.js
+++ test/free.js
@@ -8,26 +8,20 @@ test('free', function (t) {
     server.listen(port);
     
     var ports = seaport.connect('localhost', port);
-    var gotPort, gotRec;
     server.on('free', function (rec) {
-        t.same(rec, gotRec);
+        t.same(rec, ps[0]);
         ports.close();
     });
     
-    ports.register('http', function (p) {
-        t.ok(p >= 10000 && p < 65536);
-        gotPort = p;
-        
-        process.nextTick(function () {
-            var ps = ports.query('http');
-            t.equal(ps.length, 1);
-            t.equal(ps[0].host, '127.0.0.1');
-            t.equal(ps[0].port, gotPort);
-            
-            gotRec = ps[0];
-            ports.free(ps[0]);
-        });
-    });
+    var p = ports.register('http');
+    t.ok(p >= 10000 && p < 65536);
+    
+    var ps = ports.query('http');
+    t.equal(ps.length, 1);
+    t.equal(ps[0].host, '127.0.0.1');
+    t.equal(ps[0].port, p);
+    
+    ports.free(ps[0]);
     
     t.on('end', function () {
         server.close();
