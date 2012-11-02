9eb0be71929145392be3531114d95bbb1eb08de0
James Halliday <mail@substack.net>
2012-11-02 02:10:23 -0700

    update the router example

--- example/router/server_b.js
+++ example/router/server_b.js
@@ -6,6 +6,4 @@ var server = http.createServer(function (req, res) {
     res.end('version 0.1.0\r\n');
 });
 
-ports.service('http@0.1.0', function (port, ready) {
-    server.listen(port, ready);
-});
+server.listen(ports.register('http@0.1.0'));
