189e19fdb97332e4d7b601891881289af5580e4b
vvo <vincent.voyer@gmail.com>
2013-02-07 00:36:03 +0100

    Add failing test for issue #23

---
+++ test/no_key_provided.js
@@ -0,0 +1,43 @@
+var test = require('tap').test;
+var seaport = require('../');
+
+var crypto = require('crypto');
+var fs = require('fs');
+var keys = [
+    {
+        private : fs.readFileSync(__dirname + '/keys/beep'),
+        public : fs.readFileSync(__dirname + '/keys/beep.pem'),
+    },
+    {
+        private : fs.readFileSync(__dirname + '/keys/boop'),
+        public : fs.readFileSync(__dirname + '/keys/boop.pem'),
+    },
+];
+
+test('reject unauthorized hosts', function (t) {
+    t.plan(1);
+
+    var server = seaport.createServer({
+        authorized : [ keys[0] ],
+        public : keys[0].public,
+        private : keys[0].private,
+    });
+    server.listen(0);
+
+    server.on('register', function (service) {
+        t.fail('registered when it should have been rejected');
+    });
+
+    var ports = seaport.connect(server.address().port);
+    ports.once('reject', function (from, msg) {
+        t.equal(msg.type, 'address');
+        t.equal(msg.node, ports.doc.id);
+    });
+
+    var port = ports.register('http');
+
+    t.on('end', function () {
+        server.close();
+        ports.close();
+    });
+});
