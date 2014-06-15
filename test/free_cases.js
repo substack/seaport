7e3d5e55f0c3dce01d2cf4c1cd9851649fccd113
Nicholas Westlake <nicholasredlin@gmail.com>
2014-06-14 23:49:35 -0500

    propagate "free" to endpoints. prevent loop when peers are connected from both sides.

--- test/free_cases.js
+++ test/free_cases.js
@@ -2,33 +2,31 @@ var test = require('tap').test
 var seaport = require('../')
 var net = require('net')
 var fs = require('fs')
+var spawn = require('child_process').spawn
 
 test('frees with no auth', function (t) {
-    var server = seaport.createServer({heartbeatInterval: 500})
-    server.listen(0)
+    var s1 = seaport.createServer({heartbeat: 50, timeout: 60})
+    s1.listen(0)
 
-    var ports = seaport.connect(server.address().port)
+    t.equal(s1.query().length, 0)
 
-    t.equal(ports.query().length, 0, 'initially empty')
+    var client = spawn('node', [__dirname+'/lib/_client.js', s1.address().port])
 
     var web = net.createServer(function (stream) {
         stream.end('hi')
     })
 
-    web.listen(ports.register('web@0.0.1'), function () {
-        t.equal(ports.query().length, 1, 'one register')
+    s1.on('register', function (service) {
+        t.equal(s1.query().length, 1)
+        client.kill()
+    })
 
-        web.close(function () {
-
-            setTimeout(function () {
-                t.equal(ports.query().length, 0, 'after close')
-                t.end()
-            }, 4000)
-        })
+    client.on('exit', function () {
+        t.equal(s1.query().length, 0)
+        t.end()
     })
 
     t.on('end', function () {
-        server.close()
-        ports.close()
+        s1.close()
     })
 })
---
+++ test/peer.js
@@ -0,0 +1,66 @@
+var test = require('tap').test
+var seaport = require('../')
+var net = require('net')
+var macgyver = require('macgyver')
+var fs = require('fs')
+
+test('peer registrations and frees propogate', function (t) {
+    var mac = macgyver()
+    var s1 = seaport.createServer()
+    var s2 = seaport.createServer()
+    s1.id = 's1'
+    s2.id = 's2'
+    s1.listen(0)
+    s2.listen(0)
+
+    s1.peer(s2.address().port)
+    // connecting from both sides shouldn't break anything
+    s2.peer(s1.address().port)
+
+    var s1reg = mac(reg)
+    var s2reg = mac(reg)
+
+    s1reg.isCalled(3, 3)
+    s2reg.isCalled(3, 3)
+    s1.on('register', s1reg)
+    s2.on('register', s2reg)
+
+    function reg (service) {}
+
+    var c1 = seaport.connect(s1.address().port)
+    var c2 = seaport.connect(s2.address().port)
+
+    t.equal(c1.query().length, 0, 'initially empty')
+    t.equal(c2.query().length, 0, 'initially empty')
+
+    var web1 = net.createServer(function (stream) { stream.end('foo') })
+
+    web1.listen(c1.register('web@0.0.0'), function () {
+        setTimeout(function () {
+            t.equal(c1.query('web').length, 1, 'c1 web')
+            t.equal(c2.query('web').length, 1, 'c2 web')
+            t.equal(s2.query('web').length, 1, 's1 web')
+            t.equal(s2.query('web').length, 1, 's2 web')
+
+            service = c2.query('web')[0]
+            c1.free(service)
+            setTimeout(function () {
+                t.equal(c1.query('web').length, 0, 'c1 free')
+                t.equal(c2.query('web').length, 0, 'c2 free')
+                t.equal(s2.query('web').length, 0, 's1 free')
+                t.equal(s2.query('web').length, 0, 's2 free')
+                t.end()
+            }, 2000)
+
+        }, 2000)
+    })
+
+    t.on('end', function () {
+        ;[s1,s2,c1,c2,web1].forEach(function (server) {
+            server.close()
+        })
+    })
+})
+
+
+
